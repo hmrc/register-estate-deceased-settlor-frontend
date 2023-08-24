4faeb2a441b2c860a9be23c648a0807920e2fcad
slavvco <13876870+slavvco@users.noreply.github.com>
2023-08-24 20:40:19 +0100

    DDCE-4175 | CSP review.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "7.20.0"
+  val bootstrapVersion = "7.21.0"
   val mongoVersion = "1.3.0"
 
   private lazy val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"             % mongoVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.16.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "7.19.0-play-28",
     "uk.gov.hmrc"             %% "domain"                         % "8.3.0-play-28",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.13.0-play-28",
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % bootstrapVersion
@@ -22,7 +22,7 @@ object AppDependencies {
     "org.jsoup"                   %  "jsoup"                    % "1.16.1",
     "org.mockito"                 %% "mockito-scala-scalatest"  % "1.17.14",
     "com.github.tomakehurst"      %  "wiremock-standalone"      % "2.27.2",
-    "wolfendale"                  %% "scalacheck-gen-regexp"    % "0.1.2",
+    "io.github.wolfendale"        %% "scalacheck-gen-regexp"    % "1.1.0",
     "com.vladsch.flexmark"        %  "flexmark-all"             % "0.64.8"
   ).map(_ % Test)
 

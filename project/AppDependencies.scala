f3ac33690c3b773c6a5f810455688fea46d432cd
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2025-05-09 11:40:22 +0100

    DDCE-4609 - updating dependency version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,14 +9,14 @@ object AppDependencies {
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "9.11.0",
     "uk.gov.hmrc"       %% "domain-play-30"                        % "10.0.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.2.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30" % mongoVersion,
-    "org.jsoup"             % "jsoup"                   % "1.19.1",
+    "org.jsoup"             % "jsoup"                   % "1.20.1",
     "org.scalatestplus"    %% "scalacheck-1-17"         % "3.2.18.0",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0"
   ).map(_ % Test)

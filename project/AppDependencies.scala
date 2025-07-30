53e2b11e62cb674efe3c8325de51061cf2cf03f8
amruta-lohote <203861386+amruta-lohote@users.noreply.github.com>
2025-07-30 16:49:05 +0100

    DDCE-6954 -Updated BootstrapVersion, MongoVersion & sbt-plugin to 9.18.0 , 2.7.0 & 3.0.8 respectively for register-estate-deceased-settlor-frontend service.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.11.0"
-  private val mongoVersion     = "2.6.0"
+  private val bootstrapVersion = "9.18.0"
+  private val mongoVersion     = "2.7.0"
 
   private lazy val compile = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,

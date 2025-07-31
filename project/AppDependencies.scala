4ec7f41b347224215a42520dc77f0d01e0f25eb9
amruta-lohote <203861386+amruta-lohote@users.noreply.github.com>
2025-07-31 10:07:47 +0100

    DDCE-6954 -Merging PR#104
    * DDCE-6954 -Updated BootstrapVersion, MongoVersion & sbt-plugin to 9.18.0 , 2.7.0 & 3.0.8 respectively for register-estate-deceased-settlor-frontend service.
    
    * DDCE-6954 -Updated jsoup version to 1.21.2

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.11.0"
-  private val mongoVersion     = "2.6.0"
+  private val bootstrapVersion = "9.18.0"
+  private val mongoVersion     = "2.7.0"
 
   private lazy val compile = Seq(
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % mongoVersion,
@@ -16,7 +16,7 @@ object AppDependencies {
   private lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"          %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"    %% "hmrc-mongo-test-play-30" % mongoVersion,
-    "org.jsoup"             % "jsoup"                   % "1.20.1",
+    "org.jsoup"             % "jsoup"                   % "1.21.1",
     "org.scalatestplus"    %% "scalacheck-1-17"         % "3.2.18.0",
     "io.github.wolfendale" %% "scalacheck-gen-regexp"   % "1.1.0"
   ).map(_ % Test)
